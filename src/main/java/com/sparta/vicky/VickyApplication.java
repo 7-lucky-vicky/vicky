57b14dd50ecfceda2691ddb798c884194dd2bfbc
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-06-04 22:42:27 +0900

    init commit

---
+++ src/main/java/com/sparta/vicky/VickyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.vicky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VickyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VickyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/vicky/VickyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.vicky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VickyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
