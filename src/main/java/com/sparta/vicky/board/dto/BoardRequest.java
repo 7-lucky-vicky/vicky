dbc00eca46593528ddfdb48639495315d55ddb00
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-06-07 18:43:47 +0900

    Refactor: Refactor codes; Board, BoardController, BoardService, BoardRepository, BoardRequest

--- src/main/java/com/sparta/vicky/board/dto/BoardRequest.java
+++ src/main/java/com/sparta/vicky/board/dto/BoardRequest.java
@@ -1,11 +1,21 @@
 package com.sparta.vicky.board.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class BoardRequest {
+
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String region;
+
+    @NotBlank
     private String address;
+
+    @NotBlank
     private String content;
+
 }
--- src/main/java/com/sparta/vicky/board/entity/Board.java
+++ src/main/java/com/sparta/vicky/board/entity/Board.java
@@ -1,25 +1,25 @@
 package com.sparta.vicky.board.entity;
 
-import com.sparta.vicky.baseEntity.Timestamped;
 import com.sparta.vicky.board.dto.BoardRequest;
+import com.sparta.vicky.comment.entity.Comment;
+import com.sparta.vicky.common.entity.Timestamped;
 import com.sparta.vicky.user.entity.User;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
+import java.util.List;
 
-
-@Getter
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Board extends Timestamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
 
     @Column(nullable = false)
     private String title;
@@ -27,31 +27,43 @@ public class Board extends Timestamped {
     @Column(nullable = false)
     private String region;
 
-    @Column
+    @Column(nullable = false)
     private String address;
 
     @Column(nullable = false)
     private String content;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
     private final List<Comment> comments = new ArrayList<>();
 
+    @Column(nullable = false)
+    private int likes;
 
-//    Long likes;
+    /**
+     * 생성자
+     */
+    public static Board create(BoardRequest request, User user) {
+        Board board = new Board(request);
+        board.setUser(user);
 
-    protected Board() {
-
+        return board;
     }
 
-    public Board(BoardRequest boardRequest, User user) {
+    private Board(BoardRequest boardRequest) {
         this.title = boardRequest.getTitle();
         this.region = boardRequest.getRegion();
         this.address = boardRequest.getAddress();
         this.content = boardRequest.getContent();
-        this.user = user;
+        this.likes = 0;
     }
 
-    /****연관관계 편의 메서드****/
+    /**
+     * 연관관계 편의 메서드
+     */
     public void setUser(User user) {
         this.user = user;
         user.addBoard(this);
@@ -62,18 +74,22 @@ public class Board extends Timestamped {
         comment.setBoard(this);
     }
 
-
+    /**
+     * 검증 메서드
+     */
     public void verify(User user) {
         if (!this.user.equals(user))
             throw new IllegalArgumentException("이 게시물의 작성자가 아닙니다!");
     }
 
-    public Board editBoard(BoardRequest request) {
+    /**
+     * 수정 메서드
+     */
+    public void update(BoardRequest request) {
         this.title = request.getTitle();
         this.region = request.getRegion();
         this.address = request.getAddress();
         this.content = request.getContent();
-        return this;
     }
 
 }
