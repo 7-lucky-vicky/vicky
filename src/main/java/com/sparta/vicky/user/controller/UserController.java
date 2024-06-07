348ce6a0239377d4880ba0d5a3fe4a1755733ac4
seongmo-yoon <momoysm@gmail.com>
2024-06-07 14:46:14 +0900

    Feat: 회원가입 기능 구현

--- src/main/java/com/sparta/vicky/user/controller/UserController.java
+++ src/main/java/com/sparta/vicky/user/controller/UserController.java
@@ -1,13 +1,48 @@
 package com.sparta.vicky.user.controller;
 
+import com.sparta.vicky.user.dto.CommonResponse;
+import com.sparta.vicky.user.dto.SignupRequest;
+import com.sparta.vicky.user.dto.SignupResponse;
+import com.sparta.vicky.user.entity.User;
 import com.sparta.vicky.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.sparta.vicky.user.controller.ControllerUtils.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
+
+    /**
+     * 회원가입
+     */
+    @PostMapping
+    public ResponseEntity<CommonResponse<?>> signup(
+            @Valid @RequestBody SignupRequest request,
+            BindingResult bindingResult
+    ) throws IllegalArgumentException {
+        // 바인딩 예외 처리
+        if (bindingResult.hasErrors()) {
+            return getFieldErrorResponseEntity(bindingResult, "Failed to signup");
+        }
+        try {
+            User user = userService.signup(request);
+            SignupResponse response = new SignupResponse(user);
+
+            return getResponseEntity(response, "Signup successful");
+        } catch (Exception e) {
+            return getBadRequestResponseEntity(e);
+        }
+    }
 
 }
