7d9992283e5b5e6bf466aa460ac0b08e94ce1c2d
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-06-11 12:18:59 +0900

    Fix: 프로필 응답 데이터 변경

--- src/main/java/com/sparta/vicky/user/dto/ProfileResponse.java
+++ src/main/java/com/sparta/vicky/user/dto/ProfileResponse.java
@@ -4,20 +4,28 @@ import com.sparta.vicky.user.entity.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 public class ProfileResponse {
 
+    private Long userId;
     private String username;
     private String name;
     private String email;
     private String introduce;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public ProfileResponse(User user) {
+        this.userId = user.getId();
         this.username = user.getUsername();
         this.name = user.getName();
         this.email = user.getEmail();
         this.introduce = user.getIntroduce();
+        this.createdAt = user.getCreatedAt();
+        this.updatedAt = user.getUpdatedAt();
     }
 
 }
